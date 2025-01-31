193b158a383ecfc1ea13e2e4a61fac5a977ca832
Bui <bui015@apps.district279.org>
2025-01-31 16:04:01 -0600

    Defined motors, made back motors follow front motors, started on controller support

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -7,11 +7,34 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrainSubsystem extends SubsystemBase {
+  // Defines real objects to the code
+  private static final Talon leftFrontMotor = new Talon(devicenumber:1);
+  private static final Talon rightFrontMotor = new Talon(devicenumber:2);
+  private static final Talon leftBackMotor = new Talon(devicenumber:3);
+  private static final Talon rightBacktMotor = new Talon(devicenumber:4);
+
+/**Creates a differential Drive Variable */  
+  private DifferentialDrive driveTrain;
   /** Creates a new DriveTrainSubsystem. */
-  public DriveTrainSubsystem() {}
+  public DriveTrainSubsystem() {
+
+    // makes the back motors do the same thing as the front motors (for tank drive or similar)
+    leftFrontMotor.addFollower(leftBackMotor);
+    rightFrontMotor.addFollower(rightBacktMotor);
+
+    //Inverts right motors
+    rightFrontMotor.setInverted(isInverted:true);
+
+    //creates class called "driveTrain"
+    driveTrain = new DifferentialDrive(leftFrontMotor, rightFrontMotor);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void tankDrive(double leftSpeed, double rightSpeed) {
+    driveTrain.tankDrive(leftSpeed, rightSpeed);
   }
 }
